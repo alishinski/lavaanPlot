e9d7060dd7f5f147405ce19cc8d8d2f410ddb2f7
Alex Lishinski <alexlishinski@gmail.com>
2018-01-23 22:23:09 -0500

    Fixed fuctionality for adding coefficient labels

--- R/lavaanPlot.R
+++ R/lavaanPlot.R
@@ -1,16 +1,33 @@
 #' Extracts the paths from the lavaan model.
 #'
 #' @param fit A model fit object of class lavaan.
-buildPaths <- function(fit){
-  regress <- fit@ParTable$op == "~"
-  latent <- fit@ParTable$op == "=~"
+#' @param coefs whether or not to include significant path coefficient values in diagram
+#' @param sig significance level for determining what significant paths are
+#' @param stand Should the coefficients being used be standardized coefficients
+buildPaths <- function(fit, coefs = coefs, sig = sig, stand = stand){
+  if(stand){
+    ParTable <- lavaan::standardizedsolution(fit)
+  } else {
+    ParTable <- fit@ParTable
+  }
+  regress <- ParTable$op == "~"
+  latent <- ParTable$op == "=~"
+  zval_reg <- ParTable$est[regress] / ParTable$se[regress]
+  pval_reg <- (1 - stats::pnorm(abs(zval_reg))) * 2
+  signif_reg <- pval_reg < sig
+  coef <- ifelse(signif_reg, round(ParTable$est[regress], digits = 2), "")
+  #penwidths <- ifelse(coefs == "", 1, 2)
   if(any(regress)){
-  regress_paths <- paste(paste(fit@ParTable$rhs[regress], fit@ParTable$lhs[regress], sep = "->"), collapse = " ")
+    if(coefs){
+      regress_paths <- paste(paste(ParTable$rhs[regress], ParTable$lhs[regress], sep = "->"), paste("[label = '", coef, "']", sep = ""), collapse = " ")
+    } else {
+      regress_paths <- paste(paste(ParTable$rhs[regress], ParTable$lhs[regress], sep = "->"), collapse = " ")
+    }
   } else {
   regress_paths <- ""
   }
   if(any(latent)) {
-  latent_paths <- paste(paste(fit@ParTable$rhs[latent], fit@ParTable$lhs[latent], sep = "->"), collapse = " ")
+  latent_paths <- paste(paste(ParTable$rhs[latent], ParTable$lhs[latent], sep = "->"), collapse = " ")
   } else {
   latent_paths <- ""
   }
@@ -53,9 +70,12 @@ buildLabels <- function(label_list){
 #' @param labels  An optionalnamed list of variable labels fit object of class lavaan.
 #' @param graph_options  A named list of graph options for Diagrammer syntax.
 #' @param node_options  A named list of node options for Diagrammer syntax.
-#' @param edge_options  A named list of edge options for Diagrammer syntax..
+#' @param edge_options  A named list of edge options for Diagrammer syntax.
+#' @param coefs whether or not to include significant path coefficient values in diagram
+#' @param sig significance level for determining what significant paths are
+#' @param stand Should the coefficients being used be standardized coefficients
 #' @return A string specifying the path diagram for \code{model}
-buildCall <- function(name = "plot", model, labels = NULL, graph_options, node_options, edge_options){
+buildCall <- function(name = "plot", model, labels = NULL, graph_options, node_options, edge_options, coefs = coefs, sig = sig, stand = stand){
   string <- ""
   string <- paste(string, "digraph", name, "{")
   string <- paste(string, "\n")
@@ -77,7 +97,7 @@ buildCall <- function(name = "plot", model, labels = NULL, graph_options, node_o
   string <- paste(string, "\n")
   string <- paste(string, "edge", "[", paste(paste(names(edge_options), edge_options, sep = " = "), collapse = ", "), "]")
   string <- paste(string, "\n")
-  string <- paste(string, buildPaths(model))
+  string <- paste(string, buildPaths(model, coefs = coefs, sig = sig, stand = stand))
   string <- paste(string, "}", sep = "\n")
   string
 }
@@ -90,11 +110,14 @@ buildCall <- function(name = "plot", model, labels = NULL, graph_options, node_o
 #' @param graph_options  A named list of graph options for Diagrammer syntax, default provided.
 #' @param node_options  A named list of node options for Diagrammer syntax, default provided.
 #' @param edge_options  A named list of edge options for Diagrammer syntax., default provided.
+#' @param coefs whether or not to include significant path coefficient values in diagram
+#' @param sig significance level for determining what significant paths are
+#' @param stand Should the coefficients being used be standardized coefficients
 #' @return A Diagrammer plot of the path diagram for \code{model}
 #' @import DiagrammeR
 #' @export
-lavaanPlot <- function(name = "plot", model, labels = NULL, graph_options = list(overlap = "true", fontsize = "10"), node_options = list(shape = "box"), edge_options = list(color = "black")){
-  plotCall <- buildCall(name = name, model = model, labels = labels, graph_options = graph_options, node_options = node_options, edge_options = edge_options)
+lavaanPlot <- function(name = "plot", model, labels = NULL, graph_options = list(overlap = "true", fontsize = "10"), node_options = list(shape = "box"), edge_options = list(color = "black"), coefs = FALSE, sig = 0.05, stand = TRUE){
+  plotCall <- buildCall(name = name, model = model, labels = labels, graph_options = graph_options, node_options = node_options, edge_options = edge_options, coefs = coefs, sig = sig, stand = stand)
   grViz(plotCall)
 }
 
