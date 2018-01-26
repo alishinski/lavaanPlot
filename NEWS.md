6bdb871dd712682ffed8b1cf826360c1edb59094
Alex Lishinski <alexlishinski@gmail.com>
2018-01-25 21:30:47 -0500

    Updated vignette and added NEWS.md file

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# lavaanPlot 0.3.0
+
+* Added support for latent variables (denoted by circles) in plots
+
+* Added support for edge labels with significant coefficients
+
+
---
+++ vignettes/Intro_to_lavaanPlot.R
@@ -0,0 +1,29 @@
+## ------------------------------------------------------------------------
+library(lavaan)
+
+model <- 'mpg ~ cyl + disp + hp
+          qsec ~ disp + hp + wt'
+
+fit <- sem(model, data = mtcars)
+summary(fit)
+
+## ------------------------------------------------------------------------
+library(lavaanPlot)
+
+lavaanPlot(model = fit, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"))
+
+## ------------------------------------------------------------------------
+labels <- list(mpg = "Miles Per Gallon", cyl = "Cylinders", disp = "Displacement", hp = "Horsepower", qsec = "Speed", wt = "Weight")
+
+lavaanPlot(model = fit, labels = labels, node_options = list(shape = "box", fontname = "Helvetica"), edge_options = list(color = "grey"))
+
+## ------------------------------------------------------------------------
+HS.model <- ' visual  =~ x1 + x2 + x3      
+textual =~ x4 + x5 + x6
+speed   =~ x7 + x8 + x9 
+'
+
+fit <- cfa(HS.model, data=HolzingerSwineford1939)
+
+lavaanPlot(model = fit, edge_options = list(color = "grey"))
+
