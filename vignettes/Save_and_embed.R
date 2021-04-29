05fa4e9b0a065ad5b67a6f6b0972183ce8c463ff
Alex Lishinski <alexlishinski@gmail.com>
2021-04-29 00:08:47 -0400

    Adding readme, updated vignettes

--- vignettes/Save_and_embed.R
+++ vignettes/Save_and_embed.R
@@ -1,21 +1,21 @@
-## ----setup, include = FALSE---------------------------------------------------
-library(lavaan)
-library(lavaanPlot)
-library(rsvg)
-library(magrittr)
-library(DiagrammeRsvg)
+## ----setup, include = FALSE, eval = FALSE-------------------------------------
+#  library(lavaan)
+#  library(lavaanPlot)
+#  library(rsvg)
+#  library(magrittr)
+#  library(DiagrammeRsvg)
 
-## -----------------------------------------------------------------------------
-model <- 'mpg ~ cyl + disp + hp
-          qsec ~ disp + hp + wt'
-
-fit <- sem(model, data = mtcars)
-
-pl <- lavaanPlot(model = fit)
-
-
-# Not run for documentation only works on pdf
-#embed_plot_pdf(pl, "plot2.pdf")
-
-save_png(pl, "plot.png")
+## ---- eval = FALSE------------------------------------------------------------
+#  model <- 'mpg ~ cyl + disp + hp
+#            qsec ~ disp + hp + wt'
+#  
+#  fit <- sem(model, data = mtcars)
+#  
+#  pl <- lavaanPlot(model = fit)
+#  
+#  # Example for pdf embed
+#  embed_plot_pdf(pl, "plot2.pdf")
+#  
+#  # Example for saving to .png
+#  save_png(pl, "plot.png")
 
